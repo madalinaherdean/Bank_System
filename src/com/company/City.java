c504907997d56669579d8b847bbe6f4f78f217ac
admin <madalina_herdean@yahoo.com>
2022-04-04 21:02:01 +0300

    Initial commit

---
+++ src/com/company/City.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class City {
+    String cityName;
+    String regionName;
+
+    public City(String cityName, String regionName) {
+        this.cityName = cityName;
+        this.regionName = regionName;
+    }
+
+    @Override
+    public String toString() {
+        return  cityName + " - " + regionName;
+    }
+}
